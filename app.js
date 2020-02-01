c2e3bfc63e5d0f14446d369da49970918d3e226d
Rae <raefeldman@gmail.com>
2020-01-31 22:46:23 -0800

    added code comments

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+// Routes
 app.get('/', (req, res) => {
   res.render('index', {projects});
 });
@@ -15,6 +16,7 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+// dynamicaly generates route by project id
 app.get('/projects/:id', (req, res, next) => {
   const projectId = req.params.id;
   const project = projects.find( ({id}) => id === +projectId);
@@ -28,14 +30,18 @@ app.get('/projects/:id', (req, res, next) => {
   }
 });
 
+// Serve static files
 app.use('/static', express.static('public'));
 
+
+// create error
 app.use((req, res, next) => {
   const err = new Error('Something went wrong!');
   err.status = 404;
   next(err); 
 })
 
+// error handler
 app.use((err, req, res, next) => {
   console.dir(`Something went wrong! Reason: ${err.status}`);
   res.locals.error = err;
@@ -43,6 +49,7 @@ app.use((err, req, res, next) => {
   next(err);
 })
 
+// serve app
 app.listen(port, () => {
   console.log(`The app is running on port ${port}`);
 })
